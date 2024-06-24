0428ed5ed188061692e6ba1e38b163e62cd48994
saroyan-sw <82140720+saroyan-sw@users.noreply.github.com>
2024-06-24 05:45:41 -0700

    Add files via upload

---
+++ my_linked_list.py
@@ -0,0 +1,46 @@
+class MyNode():
+    def  __init__(self):
+        self.value = None 
+        self.next = None
+
+
+class LinkedList():
+    def __init__(self):
+        self.root = None
+    
+    def add(value:int):
+        '''
+        Adding new element to the end of LL
+
+        Return: None
+        '''
+    
+    def find(value:int):
+        '''
+        Finding the index of first element with that value
+
+        Return: int
+
+        '''
+
+    def get(index:int):
+        '''
+        Getting value of element with given index
+
+        Return: int
+        '''
+
+    def traverse():
+        '''
+        Returning all elents inthe LL
+
+        Return: str
+        '''
+
+    def delete(index:int):
+        '''
+        Deleting the elemnt of LL with given index
+
+        Return: None
+        '''
+    
